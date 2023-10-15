716fd6ac01abbf77299f1b9d1936abbf0678a4e3
Rahul4506 <mrrahul4506@gmail.com>
2023-10-15 14:07:43 +0530

    New

---
+++ src/config/iex.js
@@ -0,0 +1,6 @@
+// using iex platform for stock data and api
+// exporting a constant object with 2 keys
+export const iex = {
+    api_token: "pk_2d297cf7f8e1457e91282f00ffcbb5b4",
+    base_url: "https://cloud.iexapis.com/stable",
+  };
